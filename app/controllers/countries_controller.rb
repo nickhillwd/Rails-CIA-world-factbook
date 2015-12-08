c230e5b69f09e5ad3e65237fa9b6860a69242566
nickhillwd <nickhillwd@gmail.com>
2015-12-08 18:08:34 +0000

    index page running with content

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,7 +1,22 @@
 class CountriesController < ApplicationController
 
   def index
-    
+    @countries = Country.all
   end
+
+  def new
+    # @team = Team.new
+  end
+
+  # def create
+  #   Team.create(team_params)
+  #   redirect_to(teams_path)
+  # end
+
+  # private
+
+  #   def team_params
+  #     params.require(:team).permit(:name, :image, :founded, :mascot, :league, :last_position, :ground)
+  #   end
 
 end
